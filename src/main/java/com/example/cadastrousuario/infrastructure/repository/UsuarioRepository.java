8d32818d76bcc31de75994368c9fe8aaf21e75b9
NandooAlmeidaa <luisfernadogomes33@gmail.com>
2025-08-20 10:25:16 -0300

    Initial commit

---
+++ src/main/java/com/example/cadastrousuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.example.cadastrousuario.infrastructure.repository;
+
+import com.example.cadastrousuario.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
